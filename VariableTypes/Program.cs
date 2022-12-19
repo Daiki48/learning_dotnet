0ced8b1f581cc7038a7420759b2b886f74eed6d1
Daiki <102207294+Daiki48@users.noreply.github.com>
2022-12-19 23:01:40 +0900

    :zap: variable types tutorial

---
+++ VariableTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// string
+string firstName = "Daiki";
+Console.WriteLine("first name is " + firstName);
+
+// integer
+int age1 = 27;
+Console.WriteLine("age is " + age1 + "(int)");
+
+string age2 = "28";
+Console.WriteLine("age is " + age2 + "(string)");
+
